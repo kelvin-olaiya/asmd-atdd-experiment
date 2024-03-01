20b5d637d4ff7df3b1d8f8072175553fcaea794b
Kelvin Olaiya <73821880+kelvin-olaiya@users.noreply.github.com>
2024-03-01 15:48:42 +0100

    Initial commit

---
+++ project/Dependecies.scala
@@ -0,0 +1,27 @@
+import sbt.*
+
+object Dependecies {
+  /*
+   * Versions
+   */
+  lazy val scalaTestVersion = "3.2.17"
+  lazy val cucumberVersion = "8.20.0"
+  /*
+   * Libraries
+   */
+  val cucumberCore = "io.cucumber" % "cucumber-core" % "7.15.0" % "test"
+  val cucumberJunit =
+    "io.cucumber" % "cucumber-junit" % cucumberVersion % "test"
+  val cucumberJvm = "io.cucumber" % "cucumber-jvm" % cucumberVersion % "test"
+  val cucumberScala = "io.cucumber" %% "cucumber-scala" % cucumberVersion % Test
+  val scalaTest = "org.scalactic" %% "scalactic" % scalaTestVersion
+  val scalactic = "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
+  val scalaTestJUnit5 =
+    "org.scalatestplus" %% "junit-5-10" % "3.2.17.0" % "test"
+  val scalaTestMockito = "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0" % "test"
+  /*
+   * Bundles
+   */
+  val scalaTestBundle: Seq[ModuleID] = Seq(scalaTest, scalactic)
+  val cucumberBundle: Seq[ModuleID] = Seq(cucumberCore, cucumberScala)
+}
